650f4634a98dc7089301a43d4b88e8e47b4cc034
amitshuklasigmoid <122515454+amitshuklasigmoid@users.noreply.github.com>
2023-02-08 12:56:05 +0530

    Update Prime.sh

--- Prime.sh
+++ Prime.sh
@@ -1,21 +1,30 @@
 echo "Enter the n "
 read n
-i=2
 
-f=0
-while test $i -le `expr $n / 2`
-    do
-
-        if test `expr $n % $i` -eq 0
-        then
-        f=1
-        fi
-        i=`expr $i + 1`
-    done
-if test $f -eq 1
-then
-echo "$n is not a prime number"
-else
-echo "$n is a prime number"
+if [-z $n]:
+then 
+    echo "please enter some input"
 fi
+
+function Prime(){
+    n=$1
+    i=2
+    f=0
+    while test $i -le `expr $n / 2`
+        do
+
+            if test `expr $n % $i` -eq 0
+            then
+            f=1
+            fi
+            i=`expr $i + 1`
+        done
+    if test $f -eq 1
+    then
+    echo "$n is not a prime number"
+    else
+    echo "$n is a prime number"
+    fi
+}
+Prime $n
 
